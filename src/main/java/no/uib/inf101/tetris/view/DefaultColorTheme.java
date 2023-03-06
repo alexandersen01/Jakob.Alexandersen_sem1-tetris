380745a40ba5c7ef46d3392d8e6d8457dee52172
Jakob <jakob.sverre@gmail.com>
2023-03-06 16:29:15 +0100

    added pretty colors :)

--- src/main/java/no/uib/inf101/tetris/view/DefaultColorTheme.java
+++ src/main/java/no/uib/inf101/tetris/view/DefaultColorTheme.java
@@ -7,13 +7,13 @@ public class DefaultColorTheme implements ColorTheme{
     @Override
     public Color getCellColor(char c) {
         Color color = switch(c) {
-            case 'I' -> Color.CYAN;
-            case 'J' -> Color.BLUE;
-            case 'L' -> Color.ORANGE;
-            case 'O' -> Color.YELLOW;
-            case 'S' -> Color.GREEN;
-            case 'T' -> Color.MAGENTA;
-            case 'Z' -> Color.RED;
+            case 'I' -> Color.decode("#63145b");
+            case 'J' -> Color.decode("#80014d");
+            case 'L' -> Color.decode("#f23553");
+            case 'O' -> Color.decode("#005169");
+            case 'S' -> Color.decode("#569fb1");
+            case 'T' -> Color.decode("#C90E9D");
+            case 'Z' -> Color.decode("#C90E0E");
             
             case 'r' -> Color.RED;
             case 'g' -> Color.GREEN;
